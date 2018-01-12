81be6e283fedc2838439920bb1c965373e526c39
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2018-01-13 07:56:53 +0900

    initial implementation

--- README.md
+++ README.md
@@ -1,16 +1,32 @@
 # ansible-role-ipfw
 
-A brief description of the role goes here.
+Manage ipfw(8) rules.
+
+Note that `/etc/rc.d/ipfw` does not support `status`. As such, the service is
+always `started` by `ansible` even when the rules has not changed.
 
 # Requirements
 
-None
+The host must have `ipfw` enabled by default. Otherwise, enabling any firewall
+kills the ssh connection unless your firewall rule is _state-less_, which is
+unlikely.
 
 # Role Variables
 
-| variable | description | default |
+| Variable | Description | Default |
 |----------|-------------|---------|
+| `ipfw_conf_file` | path to rule file written in `sh(1)` | `/etc/ipfw.conf` |
+| `ipfw_extra_enables` | a list of extra `rc.conf(5)` variables | `["dummynet_enable", "firewall_nat_enable", "firewall_logif"]` |
+| `ipfw_service` | Service name of `ipfw` | `ipfw` |
+| `ipfw_rules` | Content of `ipfw_conf_file` | see below |
 
+## `ipfw_rules`
+
+```sh
+fwcmd="/sbin/ipfw"
+${fwcmd} -f flush
+${fwcmd} add 65000 pass all from any to any
+```
 
 # Dependencies
 
@@ -19,6 +35,16 @@ None
 # Example Playbook
 
 ```yaml
+- hosts: localhost
+  roles:
+    - ansible-role-ipfw
+  vars:
+    ipfw_rules: |
+      fwcmd="/sbin/ipfw"
+      ${fwcmd} -f flush
+
+      ${fwcmd} add 10000 deny all from any to 8.8.4.4
+      ${fwcmd} add 65000 pass all from any to any
 ```
 
 # License
--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -1,58 +1,74 @@
 require "spec_helper"
 require "serverspec"
 
-package = "ipfw"
 service = "ipfw"
-config  = "/etc/ipfw/ipfw.conf"
-user    = "ipfw"
-group   = "ipfw"
-ports   = [PORTS]
-log_dir = "/var/log/ipfw"
-db_dir  = "/var/lib/ipfw"
-
-case os[:family]
-when "freebsd"
-  config = "/usr/local/etc/ipfw.conf"
-  db_dir = "/var/db/ipfw"
-end
-
-describe package(package) do
-  it { should be_installed }
-end
+config  = "/etc/ipfw.conf"
+default_user = "root"
+default_group = "wheel"
 
 describe file(config) do
+  it { should exist }
   it { should be_file }
-  its(:content) { should match Regexp.escape("ipfw") }
+  it { should be_owned_by default_user }
+  it { should be_grouped_into default_group }
+  it { should be_mode 600 }
+  its(:content) { should match Regexp.escape("fwcmd=\"/sbin/ipfw\"") }
+  its(:content) { should match Regexp.escape("${fwcmd} -f flush") }
+  its(:content) { should match Regexp.escape("${fwcmd} add 65000 pass all from any to any") }
 end
 
-describe file(log_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
+describe command "ifconfig ipfw0" do
+  its(:exit_status) { should eq 0 }
+  its(:stderr) { should eq "" }
+  its(:stdout) { should match(/^ipfw0: flags=\d+<UP,/) }
 end
 
-describe file(db_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
+[
+  "firewall_enable",
+  "dummynet_enable",
+  "firewall_nat_enable",
+  "firewall_logif"
+].each do |i|
+  describe file "/etc/rc.conf" do
+    its(:content) { should match(/^#{Regexp.escape(i)}="YES"$/) }
+  end
 end
 
-case os[:family]
-when "freebsd"
-  describe file("/etc/rc.conf.d/ipfw") do
-    it { should be_file }
+describe file "/etc/rc.conf" do
+  its(:content) { should match(/^firewall_script="#{Regexp.escape(config)}"$/) }
+end
+
+[
+  "ipfw",
+  "dummynet",
+  "ipfw_nat"
+].each do |kmod|
+  describe command "kldstat -n #{kmod}" do
+    its(:exit_status) { should eq 0 }
+    its(:stderr) { should eq "" }
+    its(:stdout) { should match(/#{kmod}.ko/) }
   end
 end
 
 describe service(service) do
-  it { should be_running }
   it { should be_enabled }
 end
 
-ports.each do |p|
-  describe port(p) do
-    it { should be_listening }
-  end
+describe command "ipfw show" do
+  its(:exit_status) { should eq 0 }
+  its(:stderr) { should eq "" }
+  its(:stdout) { should match(/^10000\s+\d+\s+\d+\s+deny ip from any to 8\.8\.4\.4$/) }
+  its(:stdout) { should match(/^65000\s+\d+\s+\d+\s+allow ip from any to any$/) }
+end
+
+describe command "drill example.org @8.8.8.8" do
+  its(:exit_status) { should eq 0 }
+  its(:stderr) { should eq "" }
+  its(:stdout) { should match(/^;; ->>HEADER<<- opcode: QUERY, rcode: NOERROR, id: \d+$/) }
+end
+
+describe command "drill example.org @8.8.4.4" do
+  its(:exit_status) { should_not eq 0 }
+  its(:stderr) { should match(/^Error: error sending query: Error creating socket$/) }
+  its(:stdout) { should eq "" }
 end
